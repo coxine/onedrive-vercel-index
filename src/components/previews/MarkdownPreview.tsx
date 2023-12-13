fb86da2570cb7d0f3bba02028337e33161d72117
coxine <59347549+coxine@users.noreply.github.com>
2023-12-13 19:07:38 +0800

    Update MarkdownPreview.tsx

--- src/components/previews/MarkdownPreview.tsx
+++ src/components/previews/MarkdownPreview.tsx
@@ -30,62 +30,62 @@ const MarkdownPreview: FC<{
   // Check if the image is relative path instead of a absolute url
   const isUrlAbsolute = (url: string | string[]) => url.indexOf('://') > 0 || url.indexOf('//') === 0
   // Custom renderer:
-  // const customRenderer = {
-  //   // img: to render images in markdown with relative file paths
-  //   img: ({
-  //     alt,
-  //     src,
-  //     title,
-  //     width,
-  //     height,
-  //     style,
-  //   }: {
-  //     alt?: string
-  //     src?: string
-  //     title?: string
-  //     width?: string | number
-  //     height?: string | number
-  //     style?: CSSProperties
-  //   }) => {
-  //     return (
-  //       // eslint-disable-next-line @next/next/no-img-element
-  //       <img
-  //         alt={alt}
-  //         src={isUrlAbsolute(src as string) ? src : `/api/?path=${parentPath}/${src}&raw=true`}
-  //         title={title}
-  //         width={width}
-  //         height={height}
-  //         style={style}
-  //       />
-  //     )
-  //   },
-  //   // code: to render code blocks with react-syntax-highlighter
-  //   code({
-  //     className,
-  //     children,
-  //     inline,
-  //     ...props
-  //   }: {
-  //     className?: string | undefined
-  //     children: ReactNode
-  //     inline?: boolean
-  //   }) {
-  //     if (inline) {
-  //       return (
-  //         <code className={className} {...props}>
-  //           {children}
-  //         </code>
-  //       )
-  //     }
+  const customRenderer = {
+    // img: to render images in markdown with relative file paths
+    img: ({
+      alt,
+      src,
+      title,
+      width,
+      height,
+      style,
+    }: {
+      alt?: string
+      src?: string
+      title?: string
+      width?: string | number
+      height?: string | number
+      style?: CSSProperties
+    }) => {
+      return (
+        // eslint-disable-next-line @next/next/no-img-element
+        <img
+          alt={alt}
+          src={isUrlAbsolute(src as string) ? src : `/api/?path=${parentPath}/${src}&raw=true`}
+          title={title}
+          width={width}
+          height={height}
+          style={style}
+        />
+      )
+    },
+    // code: to render code blocks with react-syntax-highlighter
+    code({
+      className,
+      children,
+      inline,
+      ...props
+    }: {
+      className?: string | undefined
+      children: ReactNode
+      inline?: boolean
+    }) {
+      if (inline) {
+        return (
+          <code className={className} {...props}>
+            {children}
+          </code>
+        )
+      }
 
-  //     const match = /language-(\w+)/.exec(className || '')
-  //     return (
-  //       <SyntaxHighlighter language={match ? match[1] : 'language-text'} style={tomorrowNight} PreTag="div" {...props}>
-  //         {String(children).replace(/\n$/, '')}
-  //       </SyntaxHighlighter>
-  //     )
-  //   },
-  // }
+      const match = /language-(\w+)/.exec(className || '')
+      return (
+        <SyntaxHighlighter language={match ? match[1] : 'language-text'} style={tomorrowNight} PreTag="div" {...props}>
+          {String(children).replace(/\n$/, '')}
+        </SyntaxHighlighter>
+      )
+    },
+  }
 
   if (error) {
     return (
